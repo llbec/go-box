fa37a546480deb46501a0071e91c429f5d614e54
pazzabec <klins@163.com>
2019-01-21 17:01:15 +0800

    update

--- command/command.go
+++ command/command.go
@@ -2,22 +2,23 @@ package command
 
 import (
 	"os"
+	"strconv"
 	"strings"
 )
 
 //Entrys a command's args and flags for application
 type Entrys struct {
 	args  []string
-	flags map[string]string
+	flags map[string]interface{}
 }
 
 //Parse get os args, and analysis
 func (ens *Entrys) Parse() {
 	ens.args = os.Args[1:]
-	ens.flags = make(map[string]string)
+	ens.flags = make(map[string]interface{})
 	for i := 0; i < len(ens.args); {
 		if strings.HasPrefix(ens.args[i], "-") == true {
-			ens.flags[ens.args[i][1:]] = ens.args[i+1]
+			ens.flags[ens.args[i][1:]] = getFlag(ens.args[i+1])
 			if i+2 == len(ens.args) {
 				ens.args = ens.args[:i]
 			} else {
@@ -27,6 +28,19 @@ func (ens *Entrys) Parse() {
 			i++
 		}
 	}
+}
+
+func getFlag(v string) interface{} {
+	if i, err := strconv.Atoi(v); err == nil {
+		return i
+	}
+	if v == "true" || v == "TRUE" {
+		return true
+	}
+	if v == "false" || v == "FALSE" {
+		return false
+	}
+	return v
 }
 
 //Len get args's length
@@ -43,9 +57,9 @@ func (ens Entrys) Args(i int) string {
 }
 
 //Flags (key string) return value of the key
-func (ens Entrys) Flags(key string) string {
+func (ens Entrys) Flags(key string) interface{} {
 	if v, exist := ens.flags[key]; exist {
 		return v
 	}
-	return ""
+	return nil
 }
